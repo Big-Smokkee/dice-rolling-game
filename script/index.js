18a7e0458444516561b837a332ee13e431f84c5d
Big-Smokkee <fardinfaraz13579@gmail.com>
2025-09-20 19:33:51 +0600

    first commit not responsive

---
+++ script/index.js
@@ -0,0 +1,19 @@
+
+function rollDice() {
+    const getNumber = document.getElementById("getNumber");
+    const diceResult = document.getElementById("diceResult");
+    const diceImages = document.getElementById("diceImages");
+    const values = [];
+    const images = [];
+    for (let i = 1; i <= getNumber.value; i++)
+    {
+        const value = Math.ceil(Math.random() * 6);
+        values.push(value);
+        images.push(`<img src="images/${value}.png">`)
+    }
+    diceResult.textContent = `dice ${values.join(', ')}`;
+
+    diceImages.innerHTML = images.join(' ');
+
+    // console.log(values);
+}
